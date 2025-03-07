b643ca0764ace5754454d78821aec83938e21148
Brad Fair <brad.fair@highperformance.tech>
2025-03-07 16:57:21 -0600

    fix jdbc copy issue

--- drivers/install.sh
+++ drivers/install.sh
@@ -9,7 +9,7 @@ for rpm in *.rpm; do
 done
 
 # Copy jdbc drivers to the correct location
-cp -r jdbc /opt/tableau/tableau_driver/jdbc
+cp -r jdbc/* /opt/tableau/tableau_driver/jdbc
 
 # Install Redshift ODBC configuration
 odbcinst -i -d -f /opt/amazon/redshiftodbcx64/odbcinst.ini
